51af8918ff9ad9c5a856a639617ae763336d4270
Aaron Wilder <element1028@gmail.com>
2018-04-26 20:46:43 -0700

    cleaning up repos (old uncommitted changes)

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,12 +7,19 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  char *dirname = ".";
   // Open directory
-
+  DIR *d = opendir(dirname);
   // Repeatly read and print entries
+  if(d == NULL) 
+  {
+    printf("Error \n");
+    exit(1);
+  }
+  struct dirent *ent;
+  ent = readdir(d);
 
   // Close directory
-
+  closedir(d);
   return 0;
 }
